262ca123384b536e33b89bb587631df20d7fd52f
lele0753 <tianlanzhishen@qq.com>
2019-11-28 10:50:22 +0800

    SHANHE面板只能打开一个

--- shCadLite/sh.Creator/CadCommands.cs
+++ shCadLite/sh.Creator/CadCommands.cs
@@ -12,28 +12,37 @@ using System.Threading.Tasks;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Windows;
 
 namespace sh.Creator
 {
     public static class CadCommands
     {
+        private static PaletteSet _paletteSet;
+
         [CommandMethod("SHANHE", CommandFlags.UsePickSet)]
         public static void Query()
         {
             try
             {
-                var psConfig = new Cad.PaletteSetConfig();
-                psConfig.Name = "sh编辑器";
-                psConfig.Width = 320;
-                psConfig.Height = 640;
-                psConfig.PaletteConfigs = new List<Cad.PaletteConfig>
+                if (_paletteSet == null)
                 {
-                    new Views.shQueryEditor().GetPaletteConfig(),
-                    new Views.shResourceBox().GetPaletteConfig()
-                };
-                var ps = new sh.Cad.PaletteSetManager();
-                ps.ShowPaletteSet(psConfig);
-
+                    var psConfig = new Cad.PaletteSetConfig();
+                    psConfig.Name = "sh编辑器";
+                    psConfig.Width = 320;
+                    psConfig.Height = 640;
+                    psConfig.PaletteConfigs = new List<Cad.PaletteConfig>
+                    {
+                        new Views.shQueryEditor().GetPaletteConfig(),
+                        new Views.shResourceBox().GetPaletteConfig()
+                    };
+                    var ps = new sh.Cad.PaletteSetManager();
+                    _paletteSet = ps.ShowPaletteSet(psConfig);
+                }
+                else if(!_paletteSet.Visible)
+                {
+                    _paletteSet.Visible = true;
+                }
             }
             catch (System.Exception ex)
             {
