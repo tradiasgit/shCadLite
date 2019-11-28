8999e564366fa1f34310a2706a854c20583ae7ab
lele0753 <tianlanzhishen@qq.com>
2019-11-28 11:04:09 +0800

    合并

--- shCadLite/sh.Creator/CadCommands.cs
+++ shCadLite/sh.Creator/CadCommands.cs
@@ -13,30 +13,39 @@ using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.Geometry;
 using System.Windows;
+using Autodesk.AutoCAD.Windows;
 
 namespace sh.Creator
 {
     public static class CadCommands
     {
+        private static PaletteSet _paletteSet;
+
         [CommandMethod("SHANHE", CommandFlags.UsePickSet)]
         public static void Query()
         {
             try
             {
-                var psConfig = new Cad.PaletteSetConfig();
-                psConfig.Name = "sh编辑器";
-                psConfig.Width = 320;
-                psConfig.Height = 640;
-                psConfig.IsDock = true;
-                psConfig.PaletteConfigs = new Dictionary<string, FrameworkElement>
+                if (_paletteSet == null)
                 {
-                    {"选择集",  new Views.shQueryEditor() },
-                     {"资源盒子",  new Views.shResourceBox() },
-                };
-                var ps = new sh.Cad.PaletteSetManager();
-                
-                ps.ShowPaletteSet(psConfig);
+                    var psConfig = new Cad.PaletteSetConfig();
+                    psConfig.Name = "sh编辑器";
+                    psConfig.Width = 320;
+                    psConfig.Height = 640;
+                    psConfig.IsDock = true;
+                    psConfig.PaletteConfigs = new Dictionary<string, FrameworkElement>
+                    {
+                        {"选择集",  new Views.shQueryEditor() },
+                         {"资源盒子",  new Views.shResourceBox() },
+                    };
+                    var ps = new sh.Cad.PaletteSetManager();
 
+                    _paletteSet = ps.ShowPaletteSet(psConfig);
+                }
+                else if (!_paletteSet.Visible)
+                {
+                    _paletteSet.Visible = true;
+                }
             }
             catch (System.Exception ex)
             {
