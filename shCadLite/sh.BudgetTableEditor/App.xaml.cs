2fb821836a3c60256f6c352248dc0cd1c66d0760
lele0753 <tianlanzhishen@qq.com>
2020-02-10 16:12:32 +0800

    新建项目

---
+++ shCadLite/sh.BudgetTableEditor/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
+using sh.BudgetTableEditor.ViewModels;
+using sh.BudgetTableEditor.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace sh.BudgetTableEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+            ViewModelLocationProvider.Register<MainWindow, MainWindowViewModel>();
+
+        }
+    }
+}
---
+++ shCadLite/sh.BudgetTableEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sh.BudgetTableEditor.ViewModels
+{
+    class MainWindowViewModel: BindableBase
+    {
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set { SetProperty(ref _text, value); }
+        }
+
+        private DelegateCommand _clickCommnd;
+        public DelegateCommand ClickCommnd =>
+            _clickCommnd ?? (_clickCommnd = new DelegateCommand(ExecuteClickCommnd));
+
+        void ExecuteClickCommnd()
+        {
+            this.Text = "Click Me！";
+        }
+
+        public MainWindowViewModel()
+        {
+            this.Text = "Hello Prism!";
+        }
+    }
+}
