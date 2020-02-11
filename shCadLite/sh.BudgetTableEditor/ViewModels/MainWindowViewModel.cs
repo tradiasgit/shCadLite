8c69b431042277c42111d15adde433aca3edcba4
lele0753 <tianlanzhishen@qq.com>
2020-02-11 15:42:34 +0800

    窗体整体结构

--- shCadLite/sh.BudgetTableEditor/ViewModels/MainWindowViewModel.cs
+++ shCadLite/sh.BudgetTableEditor/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,45 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using sh.BudgetTableEditor.Views.UserControls;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows;
+using MessageBox = HandyControl.Controls.MessageBox;
 
 namespace sh.BudgetTableEditor.ViewModels
 {
     class MainWindowViewModel: BindableBase
     {
+        private ObservableCollection<LeftMenuItem> _leftMenuItems;
+
+        public ObservableCollection<LeftMenuItem> LeftMenuItems
+        {
+            get { return _leftMenuItems; }
+            set { SetProperty(ref _leftMenuItems, value); }
+        }
+
+        private object _subContent;
+
+        public object SubContent
+        {
+            get { return _subContent; }
+            set { SetProperty(ref _subContent, value); }
+        }
+
+
+        private string _contentTitle;
+
+        public string ContentTitle
+        {
+            get { return _contentTitle; }
+            set { SetProperty(ref _contentTitle, value); }
+        }
+
+
+
+
         private string _text;
         public string Text
         {
@@ -27,6 +59,47 @@ namespace sh.BudgetTableEditor.ViewModels
         public MainWindowViewModel()
         {
             this.Text = "Hello Prism!";
+            LeftMenuItems = new ObservableCollection<LeftMenuItem>();
+            LeftMenuItems.Add(new LeftMenuItem { Name = "预算",ImagePath= "\xf1ec" });
+            LeftMenuItems.Add(new LeftMenuItem { Name = "变量", ImagePath = "\xf0ce" });
         }
+
+
+
+
+        public DelegateCommand<object> SwitchContent
+        {
+            get
+            {
+                return new DelegateCommand<object>(p => 
+                {
+                    if(p is LeftMenuItem lmi)
+                    {
+                        switch (lmi.Name)
+                        {
+                            case "预算":
+                                ContentTitle = "预算表";
+                                SubContent = new BudgetItemTable();
+                                break;
+                            case "变量":
+                                ContentTitle = "变量表";
+                                SubContent = new BudgetVarTable();
+                                break;
+                        }
+
+                        //MessageBox.Show(lmi.Name, "标题", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    }
+                });
+            }
+        }
+
     }
+
+    class LeftMenuItem
+    {
+        public string Name { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+
 }
--- shCadLite/sh.BudgetTableEditor/Views/UserControls/BudgetVarTable.xaml.cs
+++ shCadLite/sh.BudgetTableEditor/Views/UserControls/BudgetVarTable.xaml.cs
@@ -1,9 +1,6 @@
-﻿using HandyControl.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,14 +11,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace sh.BudgetTableEditor.Views
+namespace sh.BudgetTableEditor.Views.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// BudgetVarTable.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : HandyControl.Controls.Window
+    public partial class BudgetVarTable : UserControl
     {
-        public MainWindow()
+        public BudgetVarTable()
         {
             InitializeComponent();
         }
