99749f2a7c30519920ba5a0f8c64310e03edca17
lele0753 <tianlanzhishen@qq.com>
2020-02-13 16:55:03 +0800

    分组编辑

--- shCadLite/sh.BudgetTableEditor/Views/UserControls/BudgetGroup/BudgetGroupEdit.xaml.cs
+++ shCadLite/sh.BudgetTableEditor/Views/UserControls/BudgetGroup/BudgetGroupEdit.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Ioc;
+using HandyControl.Data;
+using sh.BudgetTableEditor.Tools;
+using sh.BudgetTableEditor.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +21,41 @@ namespace sh.BudgetTableEditor.Views.UserControls
     /// </summary>
     public partial class BudgetGroupEdit : Window
     {
+        BudgetGroupEditViewModel ViewModel { get; set; } = new BudgetGroupEditViewModel();
+
         public BudgetGroupEdit()
         {
             InitializeComponent();
+
+            txtName.VerifyFunc += VerifyName;
+
+            DataContext = ViewModel;
         }
+
+       
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtName.VerifyData())
+                ViewModel.SaveBudgetGroup.Execute(null);
+        }
+
+        #region 验证
+        public OperationResult<bool> VerifyName(string text)
+        {
+            OperationResult<bool> result = OperationResult.Success();
+            if (string.IsNullOrWhiteSpace(text))
+                return OperationResult.Failed("不能为空");
+            // 唯一验证
+            if (text != ViewModel.OldName)
+            {
+                var btfh = SimpleIoc.Default.GetInstance<BudgetTableFileHelper>();
+                if (btfh.BudgetGroupExists(text))
+                    return OperationResult.Failed("名称重复");
+            }
+            return result;
+        }
+       
+        #endregion
     }
 }
